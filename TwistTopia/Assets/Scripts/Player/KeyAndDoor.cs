9e9ae399809a457bf6174331984e8b547d00d001
CharlieYin <315099409@qq.com>
2023-10-14 14:27:45 -0700

    Add KeyAndDoor file

--- TwistTopia/Assets/Scripts/Player/KeyAndDoor.cs
+++ TwistTopia/Assets/Scripts/Player/KeyAndDoor.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class keydoor : MonoBehaviour
+public class KeyAndDoor : MonoBehaviour
 {
     // player action keyCode
     public KeyCode pickUpKeyCode;
