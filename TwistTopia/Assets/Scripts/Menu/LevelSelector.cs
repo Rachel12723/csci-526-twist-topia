4b780002a57f373cc3664f360e8eb3f37e07fbe1
ffApril <747840349@qq.com>
2023-10-16 10:17:24 -0700

    switch scene

--- TwistTopia/Assets/Scripts/Menu/LevelSelector.cs
+++ TwistTopia/Assets/Scripts/Menu/LevelSelector.cs
@@ -6,9 +6,25 @@ public class LevelSelector : MonoBehaviour
 {
     public Button[] levelButtons;
     public string sceneName;
+    private int level;
 
     void Start()
     {
+
+        //int level;
+
+        if (PlayerPrefs.HasKey("Level"))
+        {
+            level = PlayerPrefs.GetInt("Level");
+        }
+        else
+        {
+            PlayerPrefs.SetInt("Level", 0);
+            level = 0;
+        }
+
+
+
         for (int i = 0; i < levelButtons.Length; i++)
         {
             int levelIndex = i;
@@ -19,20 +35,23 @@ public class LevelSelector : MonoBehaviour
 
     void LoadLevel(int levelIndex)
     {
-        if (levelIndex == 0)
+        if (level >= levelIndex)
         {
-            SceneManager.LoadScene("Level_1");
-            Time.timeScale = 1f;
-        }
-        else if (levelIndex == 1)
-        {
-            SceneManager.LoadScene("Level_2");
-            Time.timeScale = 1f;
-        }
-        else if (levelIndex == 2)
-        {
-            SceneManager.LoadScene("Level_3");
-            Time.timeScale = 1f;
+            if (levelIndex == 0)
+            {
+                SceneManager.LoadScene("Level_1");
+                Time.timeScale = 1f;
+            }
+            else if (levelIndex == 1)
+            {
+                SceneManager.LoadScene("Level_2");
+                Time.timeScale = 1f;
+            }
+            else if (levelIndex == 2)
+            {
+                SceneManager.LoadScene("Level_3");
+                Time.timeScale = 1f;
+            }
         }
         /* if (sceneName == "Level_3")
          {
