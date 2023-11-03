bb8e0009151411b2963272f036461cf3106eb450
ffApril <747840349@qq.com>
2023-11-02 17:09:00 -0700

    oprional menu

--- TwistTopia/Assets/Scripts/Instrction/InstructControl.cs
+++ TwistTopia/Assets/Scripts/Instrction/InstructControl.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class InstructControl : MonoBehaviour
 {
@@ -8,25 +9,31 @@ public class InstructControl : MonoBehaviour
     public GameObject wasd;
     public GameObject ad;
     public CameraState cameraState;
+    private bool show = false;
     //private FacingDirection direction;
     // Start is called before the first frame update
     void Start()
     {
+        wasd.SetActive(false);
+        ad.SetActive(false);
         StartCoroutine(HidePanelAfterSeconds(1.5f));
     }
 
     // Update is called once per frame
     void Update()
     {
-       if (cameraState.GetFacingDirection() == FacingDirection.Front)
-       {
-            wasd.SetActive(false);
-            ad.SetActive(true);
-        }
-        else
+        if (show)
         {
-            wasd.SetActive(true);
-            ad.SetActive(false);
+            if (cameraState.GetFacingDirection() == FacingDirection.Front)
+            {
+                wasd.SetActive(false);
+                ad.SetActive(true);
+            }
+            else
+            {
+                wasd.SetActive(true);
+                ad.SetActive(false);
+            }
         }
             
     }
@@ -35,5 +42,17 @@ public class InstructControl : MonoBehaviour
         panel.SetActive(true);
         yield return new WaitForSeconds(seconds); 
         panel.SetActive(false); 
+    }
+
+    public void OnButtonClick()
+    {
+        show = !show;
+        Debug.Log(show);
+        if (!show)
+        {
+            wasd.SetActive(false);
+            ad.SetActive(false);
+        }
+        EventSystem.current.SetSelectedGameObject(null);
     }
 }
