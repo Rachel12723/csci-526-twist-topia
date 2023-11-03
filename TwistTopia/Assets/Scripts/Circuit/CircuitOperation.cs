891dfc9817fd4cf8e44600a9b2e8a54145354ebe
jiananti <jiananti@usc.edu>
2023-11-02 18:15:24 -0700

    update check circuit completed

--- TwistTopia/Assets/Scripts/Circuit/CircuitOperation.cs
+++ TwistTopia/Assets/Scripts/Circuit/CircuitOperation.cs
@@ -7,9 +7,15 @@ public class CircuitOperation : MonoBehaviour
     private CircuitManager circuitManager;
     private Transform player;
     private CameraState cameraState;
+    // button for rotation
     public KeyCode rotateCircuitCode;
     public CircuitType type;
+    // list for rotatable circuits
     public List<Transform> rotatableCircuits;
+    // degrees when circuit rotates
+    private float rotateDegrees = 90.0f;
+    // record if circuit is completed
+    private bool circuitCompleted = false;
     
     
     // Start is called before the first frame update
@@ -24,8 +30,9 @@ public class CircuitOperation : MonoBehaviour
     void Update()
     {
         RotateCircuit();
+        checkCircuitCompleted();
     }
-
+    // rotate the rotatable circuits if player stands nearby and pushes button
     private void RotateCircuit()
     {
         if (Input.GetKeyDown(rotateCircuitCode))
@@ -39,13 +46,77 @@ public class CircuitOperation : MonoBehaviour
                         if (Mathf.Abs(player.transform.position.x - rotatableCircuit.position.x) < 0.5f && 
                             Mathf.Abs(player.transform.position.y - rotatableCircuit.position.y) < 1.2f)
                         {
-                            rotatableCircuit.Rotate(Vector3.up, 90.0f);
+                            rotatableCircuit.Rotate(Vector3.up, rotateDegrees);
                             break;
                         }
                     }
                 }
             }
-            
+
+            if (type == CircuitType.Front)
+            {
+                if (cameraState.GetFacingDirection() == FacingDirection.Up)
+                {
+                    foreach (Transform rotatableCircuit in rotatableCircuits)
+                    {
+                        if (Mathf.Abs(player.transform.position.x - rotatableCircuit.position.x) < 1.2f &&
+                            Mathf.Abs(player.transform.position.z - rotatableCircuit.position.z) < 1.2f)
+                        {
+                            rotatableCircuit.Rotate(Vector3.forward, rotateDegrees);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    // check if circuit is completed in specific view
+    private void checkCircuitCompleted()
+    {
+        if (type == CircuitType.Up)
+        {
+            if (cameraState.GetFacingDirection() == FacingDirection.Up)
+            {
+                bool judgeZero = true;
+                foreach (Transform rotatableCircuit in rotatableCircuits)
+                {
+                    Vector3 eulerAngles = rotatableCircuit.rotation.eulerAngles;
+                    float yRotation = eulerAngles.y;
+                    if (yRotation != 0.0f)
+                    {
+                        judgeZero = false;
+                        break;
+                    }
+                }
+
+                if (judgeZero && !circuitCompleted)
+                {
+                    circuitCompleted = true;
+                    Debug.Log("Circuit Completed!!");
+                }
+            }
+        }
+        if (type == CircuitType.Front)
+        {
+            if (cameraState.GetFacingDirection() == FacingDirection.Front)
+            {
+                bool judgeZero = true;
+                foreach (Transform rotatableCircuit in rotatableCircuits)
+                {
+                    Vector eulerAngles = rotatableCircuit.rotation.eulerAngles;
+                    float zRotation = eulerAngles.z;
+                    if (zRotation != 0.0f)
+                    {
+                        judgeZero = false;
+                        break;
+                    }
+                }
+                if (judgeZero && !circuitCompleted)
+                {
+                    circuitCompleted = true;
+                    Debug.Log("Circuit Completed!!");
+                }
+            }
         }
     }
 }
